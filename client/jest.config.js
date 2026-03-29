02418fe9c76c71ccc67a762d5bd4ff540629a8f5
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2026-03-29 13:00:15 -0400

    Fix for tests in CICD

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,20 +1,20 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  roots: ["<rootDir>/src"],
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
-  transformIgnorePatterns: [
-    "/node_modules/(?!(axios|@testing-library|styled-components)/)"
-  ],
-  moduleFileExtensions: ["js", "jsx"],
-  testMatch: [
-    "<rootDir>/src/__tests__/**/*.test.js",
-    "<rootDir>/src/__tests__/**/*.spec.js"
-  ],
-  testRegex: "src/__tests__/.*\\.(test|spec)\\.js$",
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
-  moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "<rootDir>/src/__mocks__/styleMock.js"
-  }
-};
+// module.exports = {
+//   testEnvironment: 'jsdom',
+//   roots: ["<rootDir>/src"],
+//   transform: {
+//     "^.+\\.jsx?$": "babel-jest",
+//   },
+//   transformIgnorePatterns: [
+//     "/node_modules/(?!(axios|@testing-library|styled-components)/)"
+//   ],
+//   moduleFileExtensions: ["js", "jsx"],
+//   testMatch: [
+//     "<rootDir>/src/__tests__/**/*.test.js",
+//     "<rootDir>/src/__tests__/**/*.spec.js"
+//   ],
+//   testRegex: "src/__tests__/.*\\.(test|spec)\\.js$",
+//   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+//   moduleNameMapper: {
+//     "\\.(css|less|scss|sass)$": "<rootDir>/src/__mocks__/styleMock.js"
+//   }
+// };
